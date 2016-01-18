37b2b552082939e2bd4273bb89b0f756f77519df
Ivan Wills <ivan.wills@gmail.com>
2016-01-18 15:12:12 +1100

    Code cleanups

--- lib/Data/Context/Loader/DB.pm
+++ lib/Data/Context/Loader/DB.pm
@@ -10,17 +10,32 @@ use Moose;
 use namespace::autoclean;
 use version;
 use Carp;
-use Scalar::Util;
-use List::Util;
-#use List::MoreUtils;
 use Data::Dumper qw/Dumper/;
 use English qw/ -no_match_vars /;
-
-extends 'Some::Thing';
+use Data::Context::Util qw/do_require/;
+use JSON::XS;
 
 our $VERSION = version->new('0.0.1');
 
+extends 'Data::Context::Loader';
 
+
+sub changed {
+    my ($self) = @_;
+
+    # check if we already have the raw data and if so that it is current
+    return -s $self->file != $self->stats->{size};
+}
+
+sub loader { confess "Not implemented!" }
+
+sub load {
+    my ($self) = @_;
+
+    my $raw = JSON->new->utf8->shrink->decode();
+
+    return $raw;
+}
 
 __PACKAGE__->meta->make_immutable;
 
@@ -30,7 +45,7 @@ __END__
 
 =head1 NAME
 
-Data::Context::Loader::DB - <One-line description of module's purpose>
+Data::Context::Loader::DB - Loads a config from a table in a database
 
 =head1 VERSION
 
@@ -41,73 +56,37 @@ This documentation refers to Data::Context::Loader::DB version 0.0.1
 
    use Data::Context::Loader::DB;
 
-   # Brief but working code example(s) here showing the most common usage(s)
-   # This section will be as far as many users bother reading, so make it as
-   # educational and exemplary as possible.
-
+   # Load data from table
+   my $file = Data::Context::Loader::DB->new(
+       table => '',
+   );
 
 =head1 DESCRIPTION
 
-A full description of the module and its features.
-
-May include numerous subsections (i.e., =head2, =head3, etc.).
-
+Loads configs found by L<Data::Context::Finder::DB> and performs checks to
+see if the configs have changed.
 
 =head1 SUBROUTINES/METHODS
 
-A separate section listing the public components of the module's interface.
-
-These normally consist of either subroutines that may be exported, or methods
-that may be called on objects belonging to the classes that the module
-provides.
+=head2 C<changed ()>
 
 Name the section accordingly.
 
-In an object-oriented module, this section should begin with a sentence (of the
-form "An object of this class represents ...") to give the reader a high-level
-context to help them understand the methods that are subsequently described.
+=head2 C<loader ($str)>
 
 
+=head2 C<load ()>
 
 
 =head1 DIAGNOSTICS
 
-A list of every error and warning message that the module can generate (even
-the ones that will "never happen"), with a full explanation of each problem,
-one or more likely causes, and any suggested remedies.
-
 =head1 CONFIGURATION AND ENVIRONMENT
-
-A full explanation of any configuration system(s) used by the module, including
-the names and locations of any configuration files, and the meaning of any
-environment variables or properties that can be set. These descriptions must
-also include details of any configuration language used.
 
 =head1 DEPENDENCIES
 
-A list of all of the other modules that this module relies upon, including any
-restrictions on versions, and an indication of whether these required modules
-are part of the standard Perl distribution, part of the module's distribution,
-or must be installed separately.
-
 =head1 INCOMPATIBILITIES
 
-A list of any modules that this module cannot be used in conjunction with.
-This may be due to name conflicts in the interface, or competition for system
-or program resources, or due to internal limitations of Perl (for example, many
-modules that use source code filters are mutually incompatible).
-
 =head1 BUGS AND LIMITATIONS
-
-A list of known problems with the module, together with some indication of
-whether they are likely to be fixed in an upcoming release.
-
-Also, a list of restrictions on the features the module does provide: data types
-that cannot be handled, performance issues and the circumstances in which they
-may arise, practical limitations on the size of data sets, special cases that
-are not (yet) handled, etc.
-
-The initial template usually just has:
 
 There are no known bugs in this module.
 
